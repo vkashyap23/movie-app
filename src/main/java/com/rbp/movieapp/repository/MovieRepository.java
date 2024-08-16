c0baa205e5c50333b9bcd4a2d15d4a3f2db30dd8
Varnika Kashyap <68629612+vkashyap23@users.noreply.github.com>
2024-08-16 12:17:14 +0530

    Add files via upload

---
+++ src/main/java/com/rbp/movieapp/repository/MovieRepository.java
@@ -0,0 +1,17 @@
+package com.rbp.movieapp.repository;
+
+import com.rbp.movieapp.models.Movie;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+public interface MovieRepository extends MongoRepository<Movie,String> {
+    List<Movie> findByMovieName(String movieName);
+
+    @Query("{'movieName' : ?0,'theatreName' : ?1}")
+    List<Movie> findAvailableTickets(String moviename,String theatreName);
+
+    void deleteByMovieName(String movieName);
+}
+
